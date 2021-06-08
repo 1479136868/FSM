a304242489638fc89706612ba153ef6514d1ffa5
EggPackage <66542282+1479136868@users.noreply.github.com>
2021-06-08 23:05:10 +0800

    Initial commit

---
+++ Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float h = 0;
+
+    private float v = 0;
+
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        if (h != 0 || v != 0)
+        {
+            transform.LookAt(transform.position + new Vector3(h, 0, v));
+
+            transform.Translate(Vector3.forward * Time.deltaTime * 5f);
+        }
+
+    }
+}
